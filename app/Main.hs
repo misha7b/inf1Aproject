8f9f26f14c72742986f5c7385808cbf76feba036
Michael Belyaev <michael7belyaev@gmail.com>
2022-11-10 11:06:43 +0000

    test colour

--- app/Main.hs
+++ app/Main.hs
@@ -2,26 +2,30 @@ module Main where
 
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
-import Data.Complex
 
+maxIter = 100
 
+pointsAsVertexes = mapM_ (\(x,y,z)->vertex$Vertex3 x y z)
 
 pointsList :: [(GLfloat, GLfloat, GLfloat)]
-pointsList = [((x/100),(y/100),0) | x <- [-100..100], y <- [-100..100]]
+pointsList = [((x/1000),(y/1000),0) | x <- [-1000..1000], y <- [-1000..1000]]
 
 
 iterCount :: (GLfloat, GLfloat, GLfloat) -> GLfloat -> GLfloat -> GLfloat -> GLfloat
-iterCount (x0, y0, z0) x y iter | x*x + y*y <= 2*2 && iter < 1000 = iterCount (x0, y0, z0) (x*x - y*y + x0) (2*x*y + y0) (iter + 1)
+iterCount (x0, y0, z0) x y iter | x*x + y*y <= 2*2 && iter < maxIter = iterCount (x0, y0, z0) (x*x - y*y + x0) (2*x*y + y0) (iter + 1)
                          | otherwise = iter
 
 validPoints :: [(GLfloat, GLfloat, GLfloat)] -> [(GLfloat, GLfloat, GLfloat)]
-validPoints lst = filter (\x -> iterCount x 0 0 0 == 1000) lst
+validPoints lst = filter (\x -> iterCount x 0 0 0 == maxIter) lst
+
+mandlebrotLst = validPoints pointsList
 
 
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   window "test"
+  clearColor $= Color4 1 1 1 1
   mainLoop
 
 window name = do
@@ -33,15 +37,48 @@ window name = do
 display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
+  currentColor $= Color4 0 0 0 1
   renderPrimitive Points $
-     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) (validPoints pointsList)
+     pointsAsVertexes mandlebrotLst
   flush
 
---test
 
 {-
 
-references:
+-----NOT WORKING-----
+
+display :: DisplayCallback
+display = do
+  clear [ ColorBuffer ]
+  currentColor $= Color4 0 0 0 1
+  renderPrimitive Points $
+     pointsAsVertexes mandlebrotLst
+  flush
+-}
+
+
+
+
+
+
+--drawPoints :: IO ()
+--drawPoints = mapM_ (\x -> putStrLn $ show i ++ ": " ++ blabla i) validPoints pointsList
+--test
+
+--colourChange :: IO ()
+--colourChange = mapM (\x ->)
+
+--color3f r g b = color $ Color3 r g (b :: GLfloat)
+
+--vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
+
+
+{-
+
+
+
+
+----REFERENCES----
 
 https://www.cs.hs-rm.de/~panitz/hopengl/skript.html#tth_sEc1.1
 https://wiki.haskell.org/OpenGLTutorial1
