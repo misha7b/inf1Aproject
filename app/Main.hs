287fb71a7f8af7c7d22d0a6e14577af9a7abc223
Michael Belyaev <michael7belyaev@gmail.com>
2022-11-10 17:30:40 +0000

    added simple colour

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,21 @@ module Main where
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
 
-maxIter = 20
+maxIter = 1000s
+
+s1 = 200
 
 pointsAsVertexes = mapM_ (\(x,y,z)->vertex$Vertex3 x y z)
 
 pointsList :: [(GLfloat, GLfloat, GLfloat)]
-pointsList = [((x/1000),(y/1000),0) | x <- [-1000..1000], y <- [-1000..1000]]
+pointsList = [((x/(s1/2)),(y/(s1/2)),0) | x <- [-(s1/2)..(s1/2)], y <- [-(s1/2)..(s1/2)]]
 
 
 iterCount :: (GLfloat, GLfloat, GLfloat) -> GLfloat -> GLfloat -> GLfloat -> GLfloat
 iterCount (x0, y0, z0) x y iter | x*x + y*y <= 2*2 && iter < maxIter = iterCount (x0, y0, z0) (x*x - y*y + x0) (2*x*y + y0) (iter + 1)
                          | otherwise = iter
 
-----worst way to generate two lists----
+----worst way imaginable to generate two lists----
 
 validPoints :: [(GLfloat, GLfloat, GLfloat)] -> [(GLfloat, GLfloat, GLfloat)]
 validPoints lst = filter (\x -> iterCount x 0 0 0 == maxIter) lst
@@ -26,16 +28,14 @@ invalidPoints lst = filter (\x -> iterCount x 0 0 0 /= maxIter) lst
 mandlebrotLst = validPoints pointsList
 notMandlebrotLst = invalidPoints pointsList
 
-
-iterList :: [(GLfloat, GLfloat, GLfloat)] -> [GLfloat]
-iterList x = [ iterCount y 0 0 0 | y <- x ]
-
---
-
---validPointsTest :: [(GLfloat, GLfloat, GLfloat)] -> [(GLfloat, GLfloat, GLfloat, Color3 GLfloat)]
---validPointsTest lst = [ | x <- lst]  
-
-
+plotColour :: (GLfloat, GLfloat, GLfloat) -> IO ()
+plotColour pnt = plot (iterCount pnt 0 0 0)
+              where 
+              plot :: GLfloat -> IO ()
+              plot x = do
+                currentColor $= Color4 (x/100) (2*x/100) (3*x/100) 1
+                renderPrimitive Points $
+                  pointsAsVertexes [pnt]
 
 main :: IO ()
 main = do
@@ -46,7 +46,7 @@ main = do
 
 window name = do
   createWindow name 
-  windowSize $= Size 800 800
+  windowSize $= Size 200 200
   displayCallback $= clear [ColorBuffer]
   displayCallback $= display
 
@@ -54,57 +54,27 @@ display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
   currentColor $= Color4 0 0 0 1
+  
   renderPrimitive Points $
-     pointsAsVertexes mandlebrotLst
+    pointsAsVertexes mandlebrotLst
+  repeatNtimes (length notMandlebrotLst) 0
   flush
 
---HSVtoRGB :: GLfloat -> Color3 GFloat
---HSVtoRGB x | x == 0 = Color3 0 0 0
+--not a loop--
 
-
--- idea --
---zip list of valid with their iter
-
-{-
-
------NOT WORKING-----
-
-display :: DisplayCallback
-display = do
-  clear [ ColorBuffer ]
-  currentColor $= Color4 0 0 0 1
-  renderPrimitive Points $
-     pointsAsVertexes mandlebrotLst
-  flush
--}
-
-
-
-
-
-
---drawPoints :: IO ()
---drawPoints = mapM_ (\x -> putStrLn $ show i ++ ": " ++ blabla i) validPoints pointsList
---test
-
---colourChange :: IO ()
---colourChange = mapM (\x ->)
-
---color3f r g b = color $ Color3 r g (b :: GLfloat)
-
---vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
-
-
-{-
-
-
-
+repeatNtimes 0 x = return ()
+repeatNtimes iter x = do
+   plotColour (notMandlebrotLst !! x)
+   repeatNtimes (iter-1) (x+1)
 
 ----REFERENCES----
+{-
 
 https://www.cs.hs-rm.de/~panitz/hopengl/skript.html#tth_sEc1.1
 https://wiki.haskell.org/OpenGLTutorial1
 https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
 
-
 -}
+
+
+
