3ca9f8e347b9d5ac66cf87df998b0353d7d30180
Michael Belyaev <michael7belyaev@gmail.com>
2022-11-18 19:51:47 +0000

    working on smooth colouring

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,21 @@ import Graphics.UI.GLUT
 import Data.Fixed
 
 
-maxIter = 1000
+maxIter = 1024
 
-s = 300
+start = -1
+end = 1
+
+width = 300
+height = width
+
+
 wSize = Size 300 300
 
-pallateSize = 50
+pallateSize = 30
 
 genPallate :: GLfloat -> [(GLfloat, GLfloat, GLfloat)]
-genPallate n = [((x*360/n),1,1) | x<- [0..n]]
+genPallate n = [((x*250/n),1,1) | x<- [1..n+1]]
 
 hsvPallate = genPallate pallateSize
 
@@ -37,31 +43,39 @@ hsvToRGB (h,s,v) | 0 <= hPrime && hPrime < 1 = calcRGB (c,x,0)
 
 
 
-smoothColouring n z = 1 + n + logBase 2 (logBase 2 (z))/ log(2)
+smoothColouring n z = n + 1 - nu
+      where 
+        logzn = log(z)
+        nu = (log(logzn/log(2)))/log(2)
 
 vertex3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
 vertex3f (x, y, z) = vertex $ Vertex3 x y z
 
 pointsList :: [(GLfloat, GLfloat, GLfloat)]
-pointsList = [((x/(s/2)),(y/(s/2)),0) | x <- [-(s/2)..(s/2)], y <- [-(s/2)..(s/2)]]
+pointsList = [ ((start + (x/width) * (end-start)),(start + (y/width) * (end-start)),0)    | x <- [0..width], y <- [0..height] ]
 
 
 mandlebrotCount :: (GLfloat, GLfloat, GLfloat) -> GLfloat -> GLfloat -> GLfloat -> (GLfloat,GLfloat)
 mandlebrotCount (x0, y0, z0) x y iter | x*x + y*y <= 4 && iter < maxIter = mandlebrotCount (x0, y0, z0) (x*x - y*y + x0 -0.5) (2*x*y + y0) (iter + 1)
-                         | otherwise = (iter,(sqrt(x*x + y*y)))
+                                      | otherwise = (iter,sqrt(x*x + y*y))
+
 
 
 juliaCount :: (GLfloat, GLfloat, GLfloat) -> GLfloat -> GLfloat -> GLfloat -> (GLfloat, GLfloat)
 juliaCount (x0, y0, z0) x y iter | x0 * x0 + y0 * y0 <= 2*2 && iter < maxIter = juliaCount ((x0 * x0 - y0 * y0 + x ), (2*x0*y0 + y), 0) x y (iter + 1)
                          | otherwise = (iter,x0)
 
+
+
+
+
 -- chooses the function to plot and the starting values
 
 fn = juliaCount
 --c1 = 0
 --c2 = 0
-c1 = -0.79
-c2 = 0.15
+c1 = 0.434
+c2 = 0.333
 
 
 --colours points in the that go to infinity black and other 
@@ -70,15 +84,23 @@ plotColour :: (GLfloat, GLfloat, GLfloat) -> IO ()
 plotColour pnt = plot (fn pnt c1 c2 0)
               where 
               plot :: (GLfloat,GLfloat) -> IO ()
-              plot (iter,y) = do
-                let action |iter == maxIter = do
+              plot (n,z) = do
+                let action |n == maxIter = do
                                 currentColor $= Color4 0 0 0 1
                                 renderPrimitive Points $
                                   vertex3f pnt
                             |otherwise = do
                                 
-                                
-                                let val =(hsvToRGB(hsvPallate !! floor(iter `mod'` 50)))
+                                let smoothVal = (smoothColouring n z)/maxIter
+
+                                --let val = hsvToRGB((0.07 + 2*smoothVal,1, 1) )
+                                --print(n)
+                                --print(smoothVal,n)
+                                --let  val = hsvToRGB(360*n/maxIter, 1, 1)
+                                --let val = (hsvToRGB(312,1,1))
+                                let val =(hsvToRGB(hsvPallate !! floor(n `mod'`pallateSize)))
+                                --let val = hsvToRGB ((smoothVal), 1, 1)
+                                --let val = hsvToRGB(((((n/maxIter)*360)**(1.5)) `mod'` 360), 1, (1))
                                 
                                 currentColor $= Color4  (chooseColour val !! 0) (chooseColour val !! 1) (chooseColour val !! 2) 1
                                 renderPrimitive Points $
@@ -117,17 +139,6 @@ repeatNtimes 0 x = return ()
 repeatNtimes iter x = do
    plotColour (pointsList !! x)
    repeatNtimes (iter-1) (x+1)
-
-
-
-
---generate HSV---
-
----HSV to RGB---
-
----orthographic projection---
-
-
 
 ----REFERENCES----
 {-
