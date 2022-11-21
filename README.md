9461576dcc9eb74641848fd3e9c59da0f16282d0
Michael Belyaev <michael7belyaev@gmail.com>
2022-11-21 00:18:38 +0000

    added commentary

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# inf1Aproject
+To run, load Main.hs in Haskell and run the main function.
+This project uses the OpenGL and GLUT libraries.
+
+Test images are in the testImg folder
+High resolution images are in the highresImg folder
--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import Graphics.UI.GLUT
 import Data.Fixed
 
 
-{- Favourite Values
+{- Examples
 
 --Seahorse--
 maxIter = 2500
@@ -15,10 +15,15 @@ shiftIm = 0.12172418
 
 
 -}
-                                                   
---the greater the zoom, the greater maxIter should be--
+
+--maximum number of escape iterations
+
 maxIter =  1500
-zoom = 0.0006 --0.0005
+
+--magnification level
+-- use values on the right for correctly scaled Mandelbrot set
+
+zoom = 0.0006 
 
 startReal = -zoom -- -2
 endReal = zoom --  0.47
@@ -26,13 +31,18 @@ endReal = zoom --  0.47
 startIm = -zoom -- -1.12
 endIm = zoom     -- 1.12
 
-shiftReal = -0.7445366   -- -0.722    --0.26            --(-0.7463)
-shiftIm = 0.1217208       -- 0.246     ---0.0017           --(0.1102)
+shiftReal = -0.7445366   
+shiftIm = 0.1217208   
+
+--width and height of image
 
 width = 300
 height = 300
 
 wSize = Size 300 300
+
+--number of colours in the pallate
+
 pallateSize = 30
 
 genPallate :: GLfloat -> [(GLfloat, GLfloat, GLfloat)]
@@ -56,6 +66,7 @@ hsvToRGB (h,s,v) | 0 <= hPrime && hPrime < 1 = calcRGB (c,x,0)
                 m = v-c
                 calcRGB (r1, g1, b1) = ((r1 + m), (g1 + m), (b1 + m))
 
+--smooth colouring algorithm
 
 smoothColouring n z = n + 1 - (log(log(z)/log(2)))/log(2)
         
@@ -63,22 +74,22 @@ smoothColouring n z = n + 1 - (log(log(z)/log(2)))/log(2)
 vertex3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
 vertex3f (x, y, z) = vertex $ Vertex3 x y z
 
-{-
-pointsList :: [(GLfloat, GLfloat, GLfloat)]
-pointsList = [ ((startReal + (x/width) * (endReal-startReal)),(startIm + (y/height) * (endIm-startIm)),0)    | x <- [0..width], y <- [0..height] ]
--}
+--generates coordinates
 
-scaledPoints :: [(GLfloat, GLfloat, GLfloat)]
-scaledPoints = [ (x,y,0)   | x <- [0..width], y <- [0..height] ]
+points :: [(GLfloat, GLfloat, GLfloat)]
+points = [ (x,y,0)   | x <- [0..width], y <- [0..height] ]
+
+--converts coordinates to complex values
 
 toComplex (x,y,z) = ((startReal + (x/width) * (endReal-startReal)),(startIm + (y/height) * (endIm-startIm)),0)
 
+--calculates escape iterations for the mandelbrot set
 
 mandelbrotCount :: (GLfloat, GLfloat, GLfloat) -> GLfloat -> GLfloat -> GLfloat -> (GLfloat,GLfloat)
 mandelbrotCount (x0, y0, z0) x y iter | x*x + y*y <= 4 && iter < maxIter = mandelbrotCount (x0, y0, z0) (x*x - y*y + x0+ shiftReal) (2*x*y + y0 + shiftIm) (iter + 1)
                                       | otherwise = (iter,sqrt(x*x + y*y))
 
-
+--calculates escape iterations for the julia set
 
 juliaCount :: (GLfloat, GLfloat, GLfloat) -> GLfloat -> GLfloat -> GLfloat -> (GLfloat, GLfloat)
 juliaCount (x0, y0, z0) x y iter | x0 * x0 + y0 * y0 <= 4 && iter < maxIter = juliaCount ((x0 * x0 - y0 * y0 + x ), (2*x0*y0 + y), 0) x y (iter + 1)
@@ -86,10 +97,15 @@ juliaCount (x0, y0, z0) x y iter | x0 * x0 + y0 * y0 <= 4 && iter < maxIter = ju
 
 
 
+---------------------------------------------------------------------------------------------
 
--- chooses the function to plot and the starting values
 
-fn = mandelbrotCount
+-- chooses the function that is being plotted
+
+fn = mandelbrotCount --juliaCount
+
+--set of complex numbers used
+--use 0 for both to plot the mandelbrot set
 
 c1 = 0
 c2 = 0
@@ -107,16 +123,13 @@ c2 = 0
 --c1 = -0.1226
 --c2 = -0.7449
 
---toComplex 
-
---colours points in the that go to infinity black and other 
+--plots and colours the points depending on their escape iterations and distance from centre
 
 plotColour :: (GLfloat, GLfloat, GLfloat) -> IO ()
 plotColour pnt = plot (fn (toComplex(pnt)) c1 c2 0)
               where 
               plot :: (GLfloat,GLfloat) -> IO ()
               plot (n,z) = do
-                --print(n)
                 let action |n == maxIter = do
                                 currentColor $= Color4 0 0 0 1
                                 renderPrimitive Points $
@@ -125,8 +138,7 @@ plotColour pnt = plot (fn (toComplex(pnt)) c1 c2 0)
                            |otherwise = do
                                 
                                 let smoothVal = (smoothColouring n z)/maxIter
-                                --print(smoothVal)
-                                
+            
                                 --1. smooth colouring
                                 let val = hsvToRGB ((smoothVal*360), 1, 1)
                                  
@@ -144,26 +156,18 @@ plotColour pnt = plot (fn (toComplex(pnt)) c1 c2 0)
                                   chooseColour :: (GLfloat, GLfloat, GLfloat) -> [GLfloat]
                                   chooseColour (x,y,z) = [x,y,z]
                                   scalePnt (x,y,z) = (((x-(width/2))/(width/2)), ((y-(height/2))/(height/2)), z)
-                                  
+                action                   
                               
                       
-                                  
-                action            
+                                    
                
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   window "test"
-  --print ( pointsList == (pointsTest scaledPoints))
   clearColor $= Color4 0 0 0 1
-  projection (0) 0 (0) 0 (0) 0
   mainLoop
 
-projection xl xu yl yu zl zu = do 
-  matrixMode $= Projection
-  loadIdentity
-  ortho xl xu yl yu zl zu
-  matrixMode $= Modelview 0
 
 window name = do
   createWindow name 
@@ -175,15 +179,17 @@ display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
   currentColor $= Color4 0 0 0 1
-  repeatNtimes (length scaledPoints) 0
+  repeatNtimes (length points) 0
   flush
 
---not a loop--
+--for loop--
 
 repeatNtimes 0 x = return ()
 repeatNtimes iter x = do
-   plotColour (scaledPoints !! x)
+   plotColour (points !! x)
    repeatNtimes (iter-1) (x+1)
+
+
 
 ----REFERENCES----
 {-
